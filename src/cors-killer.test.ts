4842aad7955162ba8861ba933e7fcee438eb9584
おんJ民 <J onjmin@users.noreply.github.com>
2025-08-31 00:04:30 +0900

    fix

--- src/cors-killer.test.ts
+++ src/cors-killer.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi, beforeEach } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { corsKiller } from "./cors-killer";
 import * as util from "./util";
 
